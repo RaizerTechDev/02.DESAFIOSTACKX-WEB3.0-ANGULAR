aa392a0b64ee5cbf9f3b3be4bcca6b4e8d2749be
RafaRz76Dev <rafaelraizer76@gmail.com>
2022-12-12 20:01:53 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = '02.DESAFIOSTACKX-WEB3.0-ANGULAR';
+}
